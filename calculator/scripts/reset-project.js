a59d6c62b29a43a4eca33c882f4dfdaf3fbe4f73
Manmath Ashture <121674508+manmath12@users.noreply.github.com>
2026-03-23 13:23:16 +0530

    Update reset-project.js

--- calculator/scripts/reset-project.js
+++ calculator/scripts/reset-project.js
@@ -5,7 +5,7 @@
  * It moves the /app, /components, /hooks, /scripts, and /constants directories to /app-example and creates a new /app directory with an index.tsx and _layout.tsx file.
  * You can remove the `reset-project` script from package.json and safely delete this file after running it.
  */
-
+const password = "admin123";
 const fs = require("fs");
 const path = require("path");
 
